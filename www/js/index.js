56df022fff3505c7eab66a360822d5312cbe2ab5
Peterson Pedroso <pemadata@gmail.com>
2018-04-20 15:41:19 -0300

    agora vai

--- www/js/index.js
+++ www/js/index.js
@@ -44,11 +44,11 @@ var app = {
             if(data.wasTapped) {
               //Notification was received on device tray and tapped by the user.
               alert(JSON.stringify(data));
-              alert('Primeiro' + data.title + data.body);
+              alert(data.title + '\n'+ data.body);
             } else {
               //Notification was received in foreground. Maybe the user needs to be notified.
               alert(JSON.stringify(data));
-              alert('Seguno' + data.title + data.body);
+              alert(data.title + '\n'+ data.body);
             }
         });
 
