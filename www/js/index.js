dd5d0d6494d3c362040896d252fc96e830d3b225
Peterson Pedroso <pemadata@gmail.com>
2018-04-24 16:24:08 -0300

    agora vai

--- www/js/index.js
+++ www/js/index.js
@@ -56,10 +56,8 @@ var app = {
         var parentElement = document.getElementById(id);
         var listeningElement = parentElement.querySelector('.listening');
         var receivedElement = parentElement.querySelector('.received');
-
         listeningElement.setAttribute('style', 'display:none;');
         receivedElement.setAttribute('style', 'display:block;');
-
         console.log('Received Event: ' + id);
     }
 };
