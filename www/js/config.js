433e25a4aac92cebf56025068503b91fdebc0484
Peterson Pedroso <pemadata@gmail.com>
2018-04-25 13:55:35 -0300

    agora vai

--- www/js/config.js
+++ www/js/config.js
@@ -1,1 +1,5 @@
 var _HOST_ = 'http://192.168.1.2/tecbiz/tecbiz.php';
+
+if (_HOST_ == 'http://192.168.1.2/tecbiz/tecbiz.php') {
+	alert('** BASE TESTE ** - ** BASE TESTE ** - ** BASE TESTE ** - ** BASE TESTE **'); 
+}
