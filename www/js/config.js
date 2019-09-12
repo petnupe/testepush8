392719a6cd768f4c478d70067e9a9af41c1b9b40
PETERSON\Peterson <peterson@tecbiz.com.br>
2019-09-12 15:32:19 -0300

    teste

--- www/js/config.js
+++ www/js/config.js
@@ -1,5 +1,5 @@
 var _HOST_ = 'http://www2.tecbiz.com.br/tecbiz/tecbiz.php';
-var _HOST_ = 'http://192.168.1.3/tecbiz/tecbiz.php';
+var _HOST_ = 'http://192.168.1.57/tecbiz/tecbiz.php';
 if (_HOST_ != 'http://www2.tecbiz.com.br/tecbiz/tecbiz.php') {
 	$('div[data-role="content"]').css('background-color', 'red');
 }
