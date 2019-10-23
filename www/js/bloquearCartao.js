0de741e972ca995a78e99b53679e5e35fe024427
PETERSON\Peterson <peterson@tecbiz.com.br>
2019-10-23 11:28:23 -0300

    auto bloqueio cartao

---
+++ www/js/bloquearCartao.js
@@ -0,0 +1,59 @@
+
+function bloquearCartao () {
+	var senha         = $('#senha').val();
+    var justificativa = $('#justificativa').val();
+    var erro         = '';
+
+	if (senha.length < 6 || senha.length < 6) {
+		erro += 'Senha incorreta!\n';
+	}
+
+    if(justificativa.length < 5) {
+        erro += 'Informe uma justificativa coerente com a situação!\n';
+    }
+
+	if (erro.length > 0) {
+       navigator.notification.alert(erro, null, 'Atenção', 'Tentar novamente');
+	} else {
+    	var data = $.localStorage.get('dados_usuario')[13]['usuario'];
+        
+    	$.ajax({
+            type: 'GET',
+            dataType: 'json',
+            url: _HOST_,
+            data: {
+                a: 'c06dd5',
+                senha: senha,
+                cartao  : data.carass,
+                justificativa : justificativa,
+                origem : 1
+            },
+            crossDomain: true,
+            error: function(xhr, status, error) {
+                navigator.notification.alert('Verifique a conexão com a internet!', null, 'Atenção', 'Tentar novamente');
+            },
+            beforeSend: function() {
+                $.mobile.loading( "show", {
+                          text: "Aguarde...",
+                          textVisible: true,
+                          theme: "a"
+                });
+            },
+            complete: function () {
+                $.mobile.loading('hide');
+            },
+            success: function(data) {
+                if(data.retorno == '0') {
+                    navigator.notification.alert('Cartão bloqueado com sucesso!');
+                	location.href = "menu.html";
+                } else {
+                    navigator.notification.alert(data[1]);
+                }
+            }
+        });
+	}
+}
+
+if ($.localStorage.get('dados_usuario')[13].usuario.situacao != 'L') {
+    $('#formBlqCartao').hide();
+}
--- www/js/config.js
+++ www/js/config.js
@@ -1,4 +1,4 @@
-var _HOST_ = 'http://www2.tecbiz.com.br/tecbiz/tecbiz.php';
+var _HOST_ = 'http://vmpeterson/tecbiz/tecbiz.php';
 //var _HOST_ = 'http://192.168.1.3/tecbiz/tecbiz.php';
 if (_HOST_ != 'http://www2.tecbiz.com.br/tecbiz/tecbiz.php') {
 	$('div[data-role="content"]').css('background-color', 'orange');
