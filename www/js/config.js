516032fb6f134e550b6623e2c8557c26a56b04f7
PETERSON\Peterson <peterson@tecbiz.com.br>
2019-11-07 16:55:45 -0300

    mensagem de erro para alteração de senha quando for alfanumerica

--- www/js/config.js
+++ www/js/config.js
@@ -4,5 +4,5 @@ var _HOST_ = 'https://www.tecbiz.com.br/tecbizMobile.php';
 
 //var _HOST_ = 'http://interno.tecbiz.com.br:8080/tecbiz/tecbizMobile.php';
 if (_HOST_ != 'http://www2.tecbiz.com.br/tecbiz/tecbiz.php') {
-	$('div[data-role="content"]').css('background-color', 'orange');
+	$('div[data-role="content"]').css('background-color', 'green');
 }
