2989ded3f888079f026c56a4865d09f1dc00c1b5
PETERSON\Peterson <peterson@tecbiz.com.br>
2020-02-19 17:47:47 -0300

    extrato anterior - primeiros arquivos

--- www/js/extrato_mes.js
+++ www/js/extrato_mes.js
@@ -1,44 +1,73 @@
 $(document).ready(function(e) {
 
     var data = $.localStorage.get('dados_usuario');
+    carregarExtrato();
 
-$.ajax({
-        type: 'GET',
-        dataType: 'json',
-        url: _HOST_,
-        data: {
-            a: '751fc0',
-            codent: data[13].usuario.codent,
-            codass: data[13].usuario.codass
-        },
-        crossDomain: true,
-        error: function(xhr, status, error) {
-            navigator.notification.alert('Verifique a conexão com a internet!', null, 'Atenção', 'Tentar novamente');
-        },
-        beforeSend: function() {
-            $.mobile.loading( "show", {
-                                        text: "Aguarde...", 
-                                        textVisible: true, 
-                                        theme: "a"
-                                      });
-        },
-        complete: function () {
-            $.mobile.loading('hide');
-        },
-        success: function(data) {
-            $('#nomeMes').append(data.mesAtual);
-			$('#tabelaExtrato').append('<tbody>');	
-			
-            if (data.extrato != null) {
-                $.each(data.extrato, function (k, v) {
-                    if (v.estornada != 1) {
-                        $('#tabelaExtrato').append('<tr><td align="left">'+v.dataCompra + '</td><td align="center">'+v.parcelas+'</td><td>'+v.nomeEstabelecimento+'</td><td align="right">'+v.valorParcelaFormatado +'</td></tr>');     
+    function carregarExtrato(mes, ano) {
+        $.ajax({
+                type: 'GET',
+                dataType: 'json',
+                url: _HOST_,
+                data: {
+                    a      : '751fc0',
+                    codent : data[13].usuario.codent,
+                    codass : data[13].usuario.codass,
+                    mesform: mes,
+                    anoform: ano
+                },
+                crossDomain: true,
+                error: function(xhr, status, error) {
+                    navigator.notification.alert('Verifique a conexão com a internet!', null, 'Atenção', 'Tentar novamente');
+                },
+                beforeSend: function() {
+                    $.mobile.loading( "show", {
+                                                text: "Aguarde...", 
+                                                textVisible: true, 
+                                                theme: "a"
+                                            });
+                },
+                complete: function () {
+                    $.mobile.loading('hide');
+                },
+                success: function(data) {
+                    var selectMeses  = geraSelectMeses(data.mes, data.ano);
+                    $('#selectMeses').html('');
+                    $('#tabelaExtrato').html('');
+                    $('#totalExtrato').html('');
+
+                    $('#selectMeses').append(selectMeses);
+                    if (data.extrato != null) {
+                        $.each(data.extrato, function (k, v) {
+                            if (v.estornada != 1) {
+                                $('#tabelaExtrato').append('<tr><td align="left">'+v.dataCompra + '</td><td align="center">'+v.parcelas+'</td><td>'+v.nomeEstabelecimento+'</td><td align="right">'+v.valorParcelaFormatado +'</td></tr>');     
+                            }
+                        });
+                    } else {
+                        $('#tabelaExtrato').append('<tr><td align="left"></td><td align="center"></td><td></td><td align="right"></td></tr>');
                     }
-    			});
-            } else {
-                $('#tabelaExtrato').append('<tr><td align="left"></td><td align="center"></td><td></td><td align="right"></td></tr>');
-            }
-            $('#totalExtrato').append('<tr><td colspan="3" align="right">Total</td><td align="right">'+data.total+'</td></tr>');    
+                    $('#totalExtrato').append('<tr><td colspan="3" align="right">Total</td><td align="right">'+data.total+'</td></tr>');    
+                }
+            });
         }
-    });
-});
+         
+        function geraSelectMeses(mesAtual, anoAtual) {
+            var select = '';
+            var nomeMeses = ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'];
+          
+            for(i = 12; i > 0; i--) {
+                select += '<option value="'+mesAtual+'/'+anoAtual+'">'+ nomeMeses[mesAtual-1]+'/'+anoAtual+'</option>';
+                mesAtual--;
+                if(mesAtual == 0) {
+                    mesAtual = 12;
+                    anoAtual--;
+                }
+            }
+            return select;
+        }
+
+       $('#selectMeses').on('change', function () {
+           let mesano = $(this).children('option:selected').val().split('/');
+            carregarExtrato(mesano[0], mesano[1]);
+        });
+    }
+);
