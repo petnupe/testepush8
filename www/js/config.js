1127234f90a5dcb5c97661b5be2c51ea77c4a70e
PETERSON\Peterson <peterson@tecbiz.com.br>
2020-02-20 11:13:30 -0300

    arquivos de teste

--- www/js/config.js
+++ www/js/config.js
@@ -1,7 +1,5 @@
 var _HOST_ = 'http://www2.tecbiz.com.br/tecbiz/tecbiz.php';
 
-_HOST_ = 'http://interno.tecbiz.com.br:8080/tecbiz/tecbiz.php';
-
 if (_HOST_ != 'http://www2.tecbiz.com.br/tecbiz/tecbiz.php') {
 	$('div[data-role="content"]').css('background-color', 'orange');
 }
